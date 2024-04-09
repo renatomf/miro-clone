442ac4b2bb44d1e76c0d9fdd77dce22c6c6b5438
renatomf <renatomar76@gmail.com>
2024-04-09 17:51:45 -0300

    Liblocks Setup

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -4,4 +4,10 @@ export const Participants = () => {
       List of users
     </div>
   );
-}
+};
+
+Participants.Skeleton = function ParticipantsSkeleton() {
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]" />
+  );
+};
--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -25,4 +25,10 @@ export const Toolbar = () => {
       </div>
     </div>
   );
+};
+
+Toolbar.Skeleton = function ToolbarSkeleton() {
+  return (
+    <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px] shadow-md rounded-md" />
+  )
 }
