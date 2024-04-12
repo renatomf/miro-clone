874c406184c2a20baec62307ddaa602d22282cf1
renatomf <renatomar76@gmail.com>
2024-04-12 15:57:27 -0300

    Other Elements

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -6,6 +6,9 @@ import { LayerType } from "@/types/canvas";
 import { useStorage } from "@/liveblocks.config";
 
 import { Rectangle } from "./rectangle";
+import { Ellipse } from "./ellipse";
+import { Text } from "./text";
+import { Note } from "./note";
 
 interface LayerPreviewProps {
   id: string;
@@ -25,6 +28,33 @@ export const LayerPreview = memo(({
   }
 
   switch (layer.type) {
+    case LayerType.Note:
+      return (
+        <Note
+          id={id}
+          layer={layer}
+          onPointerDown={onLayerPointerDown}
+          selectionColor={selectionColor}
+        />
+      )
+    case LayerType.Text:
+      return (
+        <Text
+          id={id}
+          layer={layer}
+          onPointerDown={onLayerPointerDown}
+          selectionColor={selectionColor}
+        />
+      )
+    case LayerType.Ellipse:
+      return (
+        <Ellipse
+          id={id}
+          layer={layer}
+          onPointerDown={onLayerPointerDown}
+          selectionColor={selectionColor}
+        />
+      );
     case LayerType.Rectangle:
       return (
         <Rectangle
