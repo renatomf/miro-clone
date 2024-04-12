3513ceb5b9b03dfa65c54f0218ada2be00e1e4d7
renatomf <renatomar76@gmail.com>
2024-04-12 11:37:40 -0300

    Color change & Deletion

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -29,6 +29,7 @@ import { connnectionIdToColor, pointerEventToCanvasPoint, resizeBounds } from "@
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
 import { SelectionBox } from "./selection-box";
+import { SelectionTools } from "./selection-tools";
 
 const MAX_LAYERS = 100;
 
@@ -300,6 +301,10 @@ export const Canvas = ({
         canUndo={canUndo}
         undo={history.undo}
         redo={history.redo}
+      />
+      <SelectionTools
+        camera={camera}
+        setLastUsedColor={setLastUsedColor}
       />
       <svg
         className="h-[100%] w-[100%]"
